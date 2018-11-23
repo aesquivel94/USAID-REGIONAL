b0bbcb1d4dcd6bba8913bfedb2e66a7ab08d176e
aesquivel94 <alejandraesqarias@gmail.com>
2018-11-23 14:31:58 -0500

    ocommented script

--- test.R
+++ test.R
@@ -1,82 +1,28 @@
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# 
+# Date: 
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# CPT predictor area optimization - Resampling.
+#
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# Packages. 
 library(tidyverse)
 library(viridis)
 
-path <- 'C:/Users/aesquivel/Desktop/USAID-Regional/CPT_R'
-setwd(path)
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# main directory path and setwd it.
+# path <- 'C:/Users/aesquivel/Desktop/USAID-Regional/CPT_R'
+# setwd(path)
 
 
-# SST <- read_delim("Feb_Mar-Apr-May.tsv",
-#            "\t", escape_double = FALSE, col_names = FALSE, 
-#            trim_ws = TRUE, skip = 2) 
+# =-=-=-=-=-=-=-=-=-=-=-= Functions.
 
 
-
-# SST_test <- SST %>%
-#   filter(row_number() != 1) %>% 
-#   filter(!grepl("cpt:field",X1)) 
-  
-  
-
-# idea <- which(is.na(SST_test$X1))[1:2]
-
-
-
-
-# =-=-=-=-=- Esta función sirve para un año. 
-
-# add_long <- function(data){
-#   Long <- data[1,] %>%
-#     t() %>% 
-#     na.omit()
-#   
-#   data[-1, ] %>%
-#     # filter(row_number() %in% (idea[1]+1):(idea[2]-1) ) %>% 
-#     setNames(., c('Lat', Long)) %>%
-#     gather(Long, SST ,-Lat) %>%
-#     mutate_if(is.character, as.numeric) %>%
-#     mutate(SST = ifelse(SST == -999, NA, SST)) 
-# }
-
-
-
-
-# =-=-=-=-=-=-= 
-
-# parte <- which(is.na(SST_test$X1))
-
-# SST_test %>%
-#   mutate(id = rep(1982:2015, each = idea[2] - idea[1])) %>% 
-#   nest(-id) %>%
-#   mutate(new_data = purrr::map(.x = data, .f = add_long)) %>% 
-#   select(-data) %>% 
-#   filter(row_number() == 1) %>% 
-#   unnest %>% 
-#   ggplot(aes(x = Long, y = Lat, fill = SST)) + 
-#   geom_tile() + 
-#   theme_bw() +  
-#   coord_fixed()
-  
-# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-# Corriendo la mala lectura de las coordenadas long 
-
-# Reading the original data set 
-SST <- read.table("Feb_Mar-Apr-May.tsv",
-                  sep="\t",  dec=".", skip =2, 
-                  fill=TRUE,na.strings =-999)
-
-# dates in CPT format 
-CPT_dates <- SST[1, -1][which(!is.na(SST[1, -1]))] 
-    
-# filter dates and rows contains cpt:field...
-SST_test <- SST %>%
-  filter(row_number() != 1) %>% 
-  filter(!grepl("cpt:field", V1)) 
-
-# Compute Length for one layer (year-raster).  
-SST_length <- which(SST_test$V1 == '')[1:2]
-
-
-# =-=-=-=-=- Esta función sirve para un año. Organiza los datos de forma Lat, Long y SST. 
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= 
+# Section 1. 
+# By one year, this function organize data set like Lat, Long and SST.
 
 add_long <- function(data){
   # data <- SST_test %>%
@@ -98,12 +44,30 @@ add_long <- function(data){
   
   return(data)}
 
-# =-=-=-=-=-=-=  
 
-# posiciones en la que se encuentra las filas para particionar. 
-partition <- which(SST_test$V1 == '')
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# Section 1. Reading original SST data set. 
 
+# Read SST...
+SST <- read.table("Feb_Mar-Apr-May.tsv",
+                  sep="\t",  dec=".", skip =2, 
+                  fill=TRUE,na.strings =-999)
 
+# extract dates in CPT format 
+CPT_dates <- SST[1, -1][which(!is.na(SST[1, -1]))] 
+    
+# filter dates and rows contains cpt:field...
+SST_test <- SST %>%
+  filter(row_number() != 1) %>% 
+  filter(!grepl("cpt:field", V1)) 
+
+# Compute Length for one layer (year-raster).  
+SST_length <- which(SST_test$V1 == '')[1:2]
+
+# This object indicate which rows have the first position like white space. 
+# partition <- which(SST_test$V1 == '')
+
+# SST transform original format to tibble (with gather).       
 test <- SST_test %>%
   tbl_df()  %>%
   mutate(id = rep(1982:2015, each = SST_length[2] - SST_length[1])) %>% 
@@ -111,33 +75,54 @@ test <- SST_test %>%
   mutate(new_data = purrr::map(.x = data, .f = add_long)) %>% 
   select(-data) 
 
-
-# test %>% 
-#   filter(row_number() < 5) %>% 
+# Option to visualize the original region.
+# test %>%
+#   filter(row_number() < 5) %>%
 #   unnest %>%
 #   ggplot(aes(x = Long, y = Lat, fill = SST) ) +
 #   geom_tile() +
-#   scale_fill_viridis() + 
+#   scale_fill_viridis() +
 #   coord_fixed() +
 #   facet_wrap(~ id) +
-#   theme_bw() 
+#   theme_bw()
 
 
-# test %>% 
-#   filter(row_number() == 1) %>% 
-#   unnest
 
-# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-# Tratando de crear los archivos de CPT. 
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# Section 2. Sampling SST and convert to CPT format.  
+# =-=-= Proof...
 
+
+# =-=-=-=-=-=-=-=
+# First step to do Resampling and CPT files. 
+
+# i guess is necesary to create a Output folder and clean the files... 
+# The other thing is necesary errase that files. 
+# One posibility is run CPT just after create one file. 
+# But in this moment isn't necesary that... it's most important in secction 3. 
+
+
+percent <- list.files(pattern = '[0-9].txt$') %>% 
+  str_split(., '_0.') %>% 
+  unlist %>% 
+  .[2] %>% 
+  str_remove('.txt') %>%
+  as.numeric() 
+             
+             
+             
+
+# Example for one layer
 data <- test  %>%
   filter(row_number() == 1) %>% 
   select(new_data) %>% 
   unnest
 
+# Rows differents to NA data. 
 try_do_this <- which(!is.na(data))
 
+# Select rows 
 rows <- sample(try_do_this, length(try_do_this)*0.2)
 
 
