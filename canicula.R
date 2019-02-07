1dfc6e104c7930833360654f35cfaf3be91872e5
aesquivel94 <alejandraesqarias@gmail.com>
2019-02-07 15:32:01 -0500

    Initial local.minima and local.maxima.

--- canicula.R
+++ canicula.R
@@ -10,27 +10,13 @@ library(tidyverse)
 library(viridis)
 library(tictoc)
 library(glue)
-
-
-#  Read data
-
-tibble(month = 1:12) %>% 
-  mutate(name = month.abb) %>% 
-  filter(month %in% 5:9)
-
-
-# prec_daily_qc <- read_csv("station_data/Data_diaria_copeco/prec_daily_qc.csv")
- 
-# prec_daily_qc %>% 
-#   filter(month %in% 5:9) %>% 
-#   View
-
 library(ncdf4)
 library(raster)
+library(lubridate)
 
 route <- 'D:/OneDrive - CGIAR/Desktop/USAID-Regional/USAID-REGIONAL/station_data/grilled/data.nc'
 
-# nc_proof <- ncdf4::nc_open(route)
+nc_chirps <- ncdf4::nc_open(route)
 # nc_proof %>% names
 # nc_proof$dim$X$vals
 # nc_proof$dim$Y$vals
@@ -38,37 +24,23 @@ route <- 'D:/OneDrive - CGIAR/Desktop/USAID-Regional/USAID-REGIONAL/station_data
 # nc_proof$dim$T$len
 
 
-
-
-
-  
-library(lubridate)
-
 # make_date(1982,1,1) + (13514 - 1)
 
+HM_shp <-  getData('GADM', country='HN', level=0)
+plot(HM_shp)
 
 
 
+HND <- stack(route) %>% 
+  flip(1) %>% 
+  crop(., HM_shp) %>% 
+  mask(., HM_shp)
 
-ajam <-  getData('GADM', country='HN', level=0)
-plot(ajam)
-
-proof <- stack(route) %>% flip(1)
-
-
-
-HND <- crop(proof, ajam)
-HND <- mask(HND, ajam)
-
-# extent(HND) <- extent(ajam)
-
-plot(HND[[1]]) 
-plot(ajam, add = TRUE)
 
 
 
 tictoc::tic()
-Prec_table <- tibble(layer = 1:nc_proof$dim$T$len) %>% 
+Prec_table <- tibble(layer = 1:nc_chirps$dim$T$len) %>% 
   mutate(date = make_date(1982,1,1) + (layer-1), julian = yday(date), 
          year = year(date), month = month(date)) %>%  
   # filter(row_number() < 2) %>% 
@@ -82,22 +54,13 @@ tictoc::toc()
 # 728.19 sec = 12.1365
 
 
-# row_1 <- 
-
-
 
 library(pracma)
-
-
 
   
 idea <- Prec_table %>% 
   unnest  %>%
   nest(-year, -id)
-  
-
-
-
   
 
 row_1 <- idea %>% 
@@ -110,19 +73,92 @@ movavg(x = row_1$prec, n = 31 , type = 't') %>% length()
 
 
 
-idea %>% 
-  filter(id == 2, year == 2018) %>% 
+test <- idea %>% 
+  filter(id == 150, year == 1994) %>% 
   unnest() %>% 
   mutate(mov = movavg(x = prec, n = 31, type = 't')) %>% 
-  filter(month %in% 6:9) %>% 
-  ggplot(aes(as.numeric(julian), mov)) + 
+  filter(month %in% 6:9) 
+
+
+
+
+test %>%
+  ggplot(aes(julian, mov)) + 
   geom_line() + 
+  geom_vline(xintercept = 255) +
+  # geom_point() +
   theme_bw()
   
   
 
 
 
+ajam <- idea %>% 
+  filter(id == 150, year == 1994) %>% 
+  unnest() %>% 
+  mutate(mov = movavg(x = prec, n = 31, type = 't')) %>% 
+  filter(month %in% 6:9) 
 
+
+
+
+
+# 
+# pixel_year <- idea %>% 
+#   filter(id == 1, year == 1982) %>% 
+#   unnest() %>% 
+#   mutate(mov = movavg(x = prec, n = 31, type = 't')) %>% 
+#   filter(month %in% 5:9) 
+# 
+# 
+# 
+# pixel_year[28, ]
+# 
+# 
+# which.min(pixel_year$mov)
+# 
+# pixel_year %>% 
+#   filter(row_number() == which.min(mov))
+
+
+
+x <- ajam$julian
+y <- ajam$mov
+
+
+in_p<-inflexi(x,y,1,length(x),5,5,plots=TRUE);in_p$an;in_p$finfl
+
+in_p %>% str
+
+
+
+library(RootsExtremaInflections)
+
+data(xydat)
+#
+#Extract x and y variables:
+#
+x=xydat$x;y=xydat$y
+#
+#Find inflection point, plot results, print Taylor coefficients and rho estimation:
+#
+d<-inflexi(x,y,1,length(x),5,5,plots=TRUE);d$an;d$finfl
+
+
+
+
+
+
+
+ajam %>%  
+  mutate(local.minima = if_else(lag(mov) > mov & lead(mov) > mov, TRUE, FALSE)) %>% 
+  mutate(local.maxima = if_else(lead(mov) < mov & lag(mov) < mov, TRUE, FALSE)) %>% 
+  filter(local.maxima == TRUE) 
+
+
+ggplot(ajam, aes(julian, mov)) + 
+  geom_line() + 
+  # geom_vline(xintercept = c(213, 238, 248, 251, 256)) 
+  geom_vline(xintercept = c(165, 237, 241, 249, 255)) 
 
 
