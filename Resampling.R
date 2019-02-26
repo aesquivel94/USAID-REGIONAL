bb8371907c714083ee11654accd4b08ba36d3e42
aesquivel94 <alejandraesqarias@gmail.com>
2019-02-26 07:56:33 -0500

    My first commit

---
+++ Resampling.R
@@ -0,0 +1,6 @@
+rm(list = ls()); gc(reset = TRUE)
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# Made by:     Alejandra Esquivel Arias. 
+# Created in:  Date: 2-2019
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# Resampling Methodology 
